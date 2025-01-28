40f77c7540360d2c238c004eb55b4be70455011e
Harshada Shashank Yadav <137686756+HarshadaSYadav@users.noreply.github.com>
2025-01-28 18:33:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Use environment variables from Vercel directly
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
